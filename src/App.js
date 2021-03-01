61d98c593a71487b0c786e9c98d6e28262dd60c2
rodrigoabam <rodrigoab.am@gmail.com>
2021-03-01 14:20:58 -0400

    Pegamendo informacoes dos filmes de uma API externa

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Tmdb from './Tmdb'
+
 
 export default () => {
+
+  const [movieList, setMovieList] = useState([]);
+
+  useEffect(()=>{
+    const loadAll = async () => {
+      let list = await Tmdb.getHomeList();
+      setMovieList(list)
+    }
+
+    loadAll()
+  },[])
+
   return (
     <div>
       <h1>
